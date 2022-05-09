2c4204019b315bd9990befc0f25168ef932a81e4
NRotondaro <niksonrotondaro1@gmail.com>
2022-05-09 14:39:01 -0300

    feat(sidebar): created sidebar and the rows with base style

---
+++ components/Sidebar.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import {
+  BellIcon,
+  HashtagIcon,
+  BookmarkIcon,
+  CollectionIcon,
+  DotsCircleHorizontalIcon,
+  MailIcon,
+  UserIcon,
+  HomeIcon,
+} from '@heroicons/react/outline'
+import SidebarRow from './SidebarRow'
+
+function Sidebar() {
+  return (
+    <div className='flex flex-col'>
+      <img
+        className="h-10 w-10"
+        src="https://logodownload.org/wp-content/uploads/2014/09/twitter-logo-4.png"
+        alt="logo"
+      />
+      <SidebarRow Icon={HomeIcon} title="Home" />
+      <SidebarRow Icon={HashtagIcon} title="Explore" />
+      <SidebarRow Icon={BellIcon} title="Notifications" />
+      <SidebarRow Icon={MailIcon} title="Messages" />
+      <SidebarRow Icon={BookmarkIcon} title="Bookmarks" />
+      <SidebarRow Icon={CollectionIcon} title="Lists" />
+      <SidebarRow Icon={UserIcon} title="Sign In" />
+      <SidebarRow Icon={DotsCircleHorizontalIcon} title="More" />
+    </div>
+  )
+}
+
+export default Sidebar
