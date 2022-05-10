9d6199b7eddde3306942d244f05df589ba9422ac
NRotondaro <niksonrotondaro1@gmail.com>
2022-05-10 20:42:05 -0300

    feat(feed): implemented the comment functionality and the refresh of those

--- typings.d.ts
+++ typings.d.ts
@@ -13,3 +13,21 @@ export type TweetBody = {
   profileImg: string
   image?: string
 }
+
+export type CommentBody = {
+  comment: string
+  tweetId: string
+  username: string
+  profileImg: string
+}
+
+export interface Comment extends CommentBody {
+  _id: string
+  _createdAt: string
+  _updatedAt: string
+  _type: 'comment'
+  tweet: {
+    _ref: string
+    _type: 'reference'
+  }
+}
